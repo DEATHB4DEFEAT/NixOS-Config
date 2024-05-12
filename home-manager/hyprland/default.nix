8b8a4a7f5cadede6f9c3f2a6bf7e1051cc9ae801
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-05-12 02:48:38 -0700

    monitor configs

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -9,6 +9,11 @@
                 "__GLX_VENDOR_LIBRARY_NAME,nvidia"
                 "WLR_NO_HARDWARE_CURSORS,1"
             ];
+
+            monitor = [
+                "HDMI-A-1,1920x1080@60,0x0,1"
+                "DP-3,3840x2160@60,1920x0,2" #TODO - XWayland force zero scaling
+            ];
         };
     };
 }
