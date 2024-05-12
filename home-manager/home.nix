8bae9faff1be35529ce615d39d26c613d4042b14
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-05-11 23:35:10 -0700

    fix mouse not existing?

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -103,6 +103,13 @@
         "ALT, mouse:272, movewindow"
         "ALT, mouse:273, resizewindow"
       ];
+
+      "env" = [
+        "LIBVA_DRIVER_NAME,nvidia"
+        "XDG_SESSION_TYPE,wayland"
+        "GBM_BACKEND,nvidia-drm"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+      ];
     };
   };
 }
