98cf9768205f49359fe27dcdfc57bdaec102e47a
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-05-19 16:49:39 -0700

    rider

--- home-manager/groups/deaths.nix
+++ home-manager/groups/deaths.nix
@@ -7,6 +7,7 @@
     home.stateVersion = "23.11";
     home.packages = with pkgs; [
         vscode
+        vscode-extensions.ms-dotnettools.csharp
         nh
         nixd
         vesktop
@@ -16,7 +17,9 @@
         obs-studio
         mpv
         krita
+        jetbrains.rider
     ];
+
     home.sessionVariables = {
         FLAKE = "/home/death/.setup";
     };
