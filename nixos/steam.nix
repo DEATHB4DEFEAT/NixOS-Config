5808465e037f283bfcb1b5c34b5ac7dcede372c6
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-05-18 17:01:38 -0700

    reformat steam flake and get gamemode

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -5,45 +5,46 @@
 }:
 
 {
-  programs = lib.mkDefault {
-    steam = {
-      enable = true;
-      package = pkgs.steam.override {
-        extraLibraries = p: with p; [
-            libunwind
-            pipewire
-            # For Space Station 14
-            openssl
-            fluidsynth
-            soundfont-fluid
-          ];
-      };
-      #gamescopeSession.enable = true;
-      remotePlay.openFirewall = true;
+    programs = lib.mkDefault {
+        steam = {
+            enable = true;
+            package = pkgs.steam.override {
+                extraLibraries = p: with p; [
+                    libunwind
+                    pipewire
+                    # For Space Station 14
+                    openssl
+                    fluidsynth
+                    soundfont-fluid
+                ];
+            };
+            #gamescopeSession.enable = true;
+            remotePlay.openFirewall = true;
+        };
+
+        # gamescope = {
+        # 	enable = true;
+        # 	capSysNice = true;
+        # 	args = [
+        # 		"--expose-wayland"
+        # 	];
+        # };
+
+        dconf.enable = true;
     };
 
-    # gamescope = {
-    # 	enable = true;
-    # 	capSysNice = true;
-    # 	args = [
-    # 		"--expose-wayland"
-    # 	];
-    # };
 
-    dconf.enable = true;
-  };
+    environment.systemPackages = with pkgs; [
+        fluidsynth
+        steamPackages.steamcmd
+        steam-tui
+        gamescope
+        gamemode
+    ];
 
 
-  environment.systemPackages = [
-    pkgs.fluidsynth
-    pkgs.steamPackages.steamcmd
-    pkgs.steam-tui
-    pkgs.gamescope
-  ];
-
-
-  environment.sessionVariables = {
-    # Steam needs this to find Proton-GE
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-  };
+    environment.sessionVariables = {
+        # Steam needs this to find Proton-GE
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    };
 }
