56d0f9f0b63999165c106dce47635dbf4d1eb176
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-08-14 05:15:21 -0700

    no elisa

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -184,6 +184,9 @@
     services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+        elisa
+    ];
 
     programs.hyprland.enable = true;
     environment.sessionVariables = {
