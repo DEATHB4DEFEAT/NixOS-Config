32760d06c88a258d0cd321be7ab8049587db2e52
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-09-02 13:22:56 -0700

    Revert "static local ip"
    This reverts commit c3c82dbafcec8605b54eae07f0efeb0750cdfb2d.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -36,15 +36,16 @@
         kernelPackages = pkgs.linuxPackages_zen;
     };
 
-    networking = {
-        hostName = "DTLinix";
 
-        networkmanager.enable = true;
-        interfaces.eth0.ipv4.addresses = [ {
-            address = "192.168.0.45";
-            prefixLength = 24;
-        } ];
-    };
+    networking.hostName = "DTLinix"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
 
 
     # Set your time zone.
