a85c7c5b2a066134dc17d90dca4728dbf350a18c
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-03-02 14:44:42 -0800

    copy/migrate some old laptop hyprland settings

--- home-manager/config/hyprland/default.nix
+++ home-manager/config/hyprland/default.nix
@@ -81,9 +81,61 @@
                 "ckb-next"
             ];
 
+            general = {
+                gaps_in = 5;
+                gaps_out = 10;
+                border_size = 2;
+                "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+                "col.inactive_border" = "rgba(595959aa)";
+                layout = "dwindle";
+            };
+
+            decoration = {
+                rounding = 8;
+
+                blur = {
+                    enabled = true;
+                    size = 3;
+                    passes = 1;
+                    vibrancy = 0.1696;
+                    new_optimizations = true;
+                };
+
+                shadow = {
+                    enabled = true;
+                    range = 4;
+                    render_power = 3;
+                    color = "rgba(1a1a1aee)";
+                };
+            };
+
+            animations = {
+                enabled = true;
+
+                bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+                animation = [
+                    "windows, 1, 7, myBezier"
+                    "windowsOut, 1, 7, default, popin 80%"
+                    "border, 1, 10, default"
+                    "borderangle, 1, 8, default"
+                    "fade, 1, 7, default"
+                    "workspaces, 1, 6, default"
+                ];
+            };
+
+            dwindle = {
+                pseudotile = true;
+                preserve_split = true;
+            };
+
             misc = {
                 middle_click_paste = false;
+                force_default_wallpaper = 0;
             };
+
+            windowrulev2 = [
+                "suppressevent maximize,class:.*"
+            ];
         };
     };
 }
