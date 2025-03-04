f528b8b35e4a8b8b486a2ee773547ea6917b0cb8
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-03-04 15:59:45 -0800

    foot config

--- home-manager/config/apps/foot.nix
+++ home-manager/config/apps/foot.nix
@@ -2,6 +2,20 @@
     programs = {
         foot = {
             enable = true;
+            settings = {
+                main = {
+                    font = "monospace:size=16";
+                    dpi-aware = "yes";
+                };
+
+                scrollback = {
+                    lines = 10000;
+                };
+
+                mouse = {
+                    hide-when-typing = "yes";
+                };
+            };
         };
     };
 }
