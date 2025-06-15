02ea6f0411ef9dbe9d7faf7c2b667a538c963766
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-06-14 17:53:53 -0700

    tv

--- home-manager/config/hyprland/display.nix
+++ home-manager/config/hyprland/display.nix
@@ -2,8 +2,9 @@
     wayland.windowManager.hyprland = {
         settings = {
             monitor = [
-                "HDMI-A-2, 1920x1080@60, 0x0, 1"
-                "DP-1, 3840x2160@60, 1920x0, 2"
+                "HDMI-A-2, 1920x1080@60, 0x0, 1" # Sceptre
+                "DP-1, 3840x2160@60, 1920x0, 2" # LG
+                "HDMI-A-1, 3840x2160@60.00, 3840x0, 2" # TV
             ];
 
             xwayland = {
