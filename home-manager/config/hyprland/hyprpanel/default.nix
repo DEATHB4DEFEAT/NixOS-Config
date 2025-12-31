d5013fefad7b585b55df60da57dbb2708ae1e7a8
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-12-31 00:03:36 -0800

    hide panel with bind

--- home-manager/config/hyprland/hyprpanel/default.nix
+++ home-manager/config/hyprland/hyprpanel/default.nix
@@ -1,6 +1,5 @@
 {
     pkgs,
-    config,
     inputs,
     ...
 }:
@@ -10,7 +9,12 @@
         inputs.hyprpanel.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
-    wayland.windowManager.hyprland.settings.exec-once = [
-        "hyprpanel"
-    ];
+    wayland.windowManager.hyprland.settings = {
+        # exec-once = [
+        #     "hyprpanel"
+        # ];
+        bind = [
+            "SUPER, bracketright, exec, pkill hyprpanel || hyprpanel" # Not ideal but works, I don't need it responsive enough to care
+        ];
+    };
 }
